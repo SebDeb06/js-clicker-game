974e670e44fb204244c435846402e3035aa1e132
SebDeb06 <sesa@hopvgskole.no>
2025-04-30 09:37:51 +0200

    You can now get points from clicking on the button.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const clickButton = document.getElementById("ClickButton");
+const valueIndicator = document.getElementById("valueIndicator");
+
 let starterValue = 0;
+
+function GetPoint(){
+    starterValue += 1
+    valueIndicator.innerText = "Points: " + starterValue
+}
+
+clickButton.addEventListener("click", GetPoint)
+

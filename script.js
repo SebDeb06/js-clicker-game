a95a19a87ed4f2a1d4c6cf9b3b4baf0eb0394421
SebDeb06 <sesa@hopvgskole.no>
2025-04-30 10:46:47 +0200

    I've added some framework for the upgrading system.

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const valueIndicator = document.getElementById("valueIndicator");
 
 let starterValue = 0;
 
+const upgrade1 = document.getElementById("upgrade1")
+
+if(starterValue == 10){
+
+}
+
 function GetPoint(){
     starterValue += 1
     valueIndicator.innerText = "Points: " + starterValue

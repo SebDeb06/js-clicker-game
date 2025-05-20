90aefd0c312f9b6c7dad36b26a3a1fad98157cf2
SebDeb06 <sesa@hopvgskole.no>
2025-05-20 10:11:32 +0200

    made the upgrade button work but it doesnt work to upgrade more than once.

--- script.js
+++ script.js
@@ -12,14 +12,32 @@ const moreClicks = document.getElementById("moreClicks");
 
 let starterValue = 0;
 
+let clickAmount = 1;
+
 if(starterValue == 10){
 
 }
 
 function GetPoint(){
-    starterValue += 1
+    starterValue += clickAmount
     valueIndicator.innerText = "Points: " + starterValue
 }
 
 clickButton.addEventListener("click", GetPoint)
 
+let clicksPrice = 10;
+
+function upgradeClicks(){
+    if(starterValue == clicksPrice){
+        clickAmount += 0.25;
+        starterValue = starterValue - clicksPrice
+        clicksPrice = clicksPrice *= 2.5;
+        moreClicksPrice.innerText = "Price: " + clicksPrice;
+        valueIndicator.innerText = "Points: " + starterValue;
+    }
+    else{
+        console.log("not enough points")
+    }
+}
+
+moreClicksButton.addEventListener("click", upgradeClicks)
